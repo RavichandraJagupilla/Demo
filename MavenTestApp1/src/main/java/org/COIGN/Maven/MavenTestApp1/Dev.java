909383b3fdeb003ebadf2b81e04a02258e528378
LENOVO <LENOVO@DESKTOP-EC0CQJN>
2023-09-24 04:49:42 +0530

    Maven Project

---
+++ MavenTestApp1/src/main/java/org/COIGN/Maven/MavenTestApp1/Dev.java
@@ -0,0 +1,10 @@
+package org.COIGN.Maven.MavenTestApp1;
+
+public class Dev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Devops Work shop");
+	}
+
+}
